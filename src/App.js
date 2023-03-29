2d474522d230c3fb68414396e17cd9c118a1c8b4
Santiago Arocha <arochas846@gmail.com>
2023-03-29 17:59:31 -0300

    passing a list through json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import category from './data/categories.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {console.log(category)}
+    <ul>
+      {
+         category.map((category) => (
+          <li key={category.category.id}>{category.category.name}</li>
+         ))
+
+  
+      }
+      
+    </ul>
+
+  </div>
   );
 }
 
