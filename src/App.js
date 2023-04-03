787081f9b033eb997c1324be096b3b38701a0875
Santiago Arocha <arochas846@gmail.com>
2023-04-03 15:06:35 -0300

    Data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import category from './data/categories.json';
+import {categories} from './data/categories';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      {console.log(category)}
-    <ul>
-      {
-         category.map((category) => (
-          <li key={category.category.id}>{category.category.name}</li>
-         ))
 
   
-      }
+  
+
+  return (
+    <div className="App">
+    <ul>
+      {
+          categories.map((category) => (
+            <li kew={category.id}>{category.name}</li>
+          ))
+        }
       
     </ul>
 
---
+++ src/data/categories.js
@@ -0,0 +1,149 @@
+
+    
+    export const categories = [
+            {
+                "id":1,
+                "name":"Bebidas",
+                "sublevels": [ 
+                    {
+                        "id":1,
+                        "name":"Gaseosas",
+                        "sublevels": [
+                    {
+                        "id": 1,
+                        "name":"Con azucar",
+                        "sublevels":[
+                            {
+                                "id":1,
+                                "name":"Pomelo"
+                            },
+                            {
+                                "id":2,
+                                "name":"Coca Cola"
+                            },
+                            {
+                                "id":3,
+                                "name":"Sprite"
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "name":"Sin azúcar"
+                    }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "name":"Con alcohol",
+                        "sublevels": [
+                            {
+                                "id":1,
+                                "name": "Vinos",
+                                "sublevels":[
+                                    {
+                                       "id":1,
+                                       "name":"Tintos" 
+                                    },
+                                    {
+                                        "id":2,
+                                        "name": "Blancos"
+                                    }
+                                ]
+                            },
+                            {
+                               "id":2,
+                               "name":"Fernet" 
+                            },
+                            {
+                               "id":3,
+                               "name":"Cerveza" 
+                            },
+                            {
+                               "id":4,
+                               "name":"Wiski" 
+                            },
+                            {
+                               "id":5,
+                               "name":"Shin" 
+                            }
+                        ]
+                    }
+                    
+                ]
+            },
+            {
+                "id":2,
+                "name":"Desayunos",
+                "sublevels":[
+                    {
+                        "id":1,
+                        "name":"Con leche",
+                        "sublevels":[
+                            {
+                                "id":1,
+                                "name":"Con avena"
+                            },
+                            {
+                                "id":2,
+                                "name":"Con cafe"
+                            },
+                            {
+                                "id":1,
+                                "name":"Con cocoa"
+                            }
+                        ]
+                    },
+                    {
+                        "id":2,
+                        "name":"Con agua",
+                        "sublevels":[
+                            {
+                                "id":1,
+                                "name":"Cafe"
+                            },
+                            {
+                                "id":2,
+                                "name":"Te"
+                            },
+                            {
+                                "id":3,
+                                "name":"Mate"
+                            }
+                        ]
+                    }
+                ]
+
+            },
+            {
+                "id":3,
+                "name":"Almuerzos",
+                "sublevels":[
+                    {
+                        "id":1,
+                        "name":"Con milanga",
+                        "sublevels":[
+                            {
+                                "id":1,
+                                "name":"De Pollo"
+                            },
+                            {
+                                "id":2,
+                                "name":"De pescado"
+                            },
+                            {
+                                "id":3,
+                                "name":"De carne"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    
+ 
+    
+    
+ 
+    
+
